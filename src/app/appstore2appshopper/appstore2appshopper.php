d407c97ffb6a83ac2242931e4dad9cace9d23a4b
U-sskaje-PC\sskaje <sskaje@sskaje-PC.(none)>
2012-07-09 17:02:58 +0800

    Add support to hidden purchased app list

--- src/app/appstore2appshopper/appstore2appshopper.php
+++ src/app/appstore2appshopper/appstore2appshopper.php
@@ -3,6 +3,7 @@ $apple_id = '';
 $apple_password = '';
 $appshopper_username = '';
 $appshopper_password = '';
+$import_hidden = false;
 
 require(__DIR__ . '/../../classes/iTunes.class.php');
 require(__DIR__ . '/../../classes/Appshopper.class.php');
@@ -15,13 +16,23 @@ $itunes->password = $apple_password;
 $itunes->guid = '11111111.22222222.00000000.44444444.CC0183D3.33019387.512309CA';
 
 $itunes->login();
+
+$app_ids = array();
 $ids = $itunes->getPurchasedIDList();
 if (!isset($ids['Apps']) || empty($ids['Apps'])) {
 	die('No purchased app found.');
 }
+$app_ids += $ids['Apps'];
+
+if ($import_hidden) {
+	$ids = $itunes->getPurchasedIDList(true);
+	if (isset($ids['Apps']) && !empty($ids['Apps'])) {
+		$app_ids += $ids['Apps'];
+	}
+}
 
 # IMPORT to myapp list only
-$appshopper->import(array_keys($ids['Apps']));
+$appshopper->import(array_keys($app_ids));
 
 # if u want to import to wishlist, use code below
 /*
